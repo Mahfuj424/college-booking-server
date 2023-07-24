a8215eb5b7bacb0c622d1d5802af4cb0cfc2bde6
mahfuj <mahfujahmad424@gmail.com>
2023-07-24 23:38:45 +0600

    final commit

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ async function run() {
     const myCollegeCollection = client
       .db("collegeBooking")
       .collection("myCollege");
+    const collegeCollection = client.db("collegeBooking").collection("college");
 
     app.get("/collegeName", async (req, res) => {
       const result = await admissionCollection.find().toArray();
@@ -44,6 +45,71 @@ async function run() {
 
       res.send(result);
     });
+
+    // app.get('/myCollege/:email', async (req, res) => {
+    //   const email = req.params.email
+    //   const result = await myCollegeCollection.findOne({ email: email })
+    //   res.send(result)
+    // });
+
+    app.get("/myCollege", async (req, res) => {
+      const result = await myCollegeCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/college", async (req, res) => {
+      const result = await collegeCollection.find().toArray();
+      res.send(result);
+    });
+
+    // app.get('/myCollege/:email', async (req, res) => {
+    //   const result = await myCollegeCollection.findOne({studentEmail: req.params.email}).toArray()
+    //   res.send(result)
+    // });
+
+    app.get("/myCollege/:email", async (req, res) => {
+      console.log(req.params.email);
+      const result = await myCollegeCollection
+        .find({ StudentEmail: req.params.email })
+        .toArray();
+      res.send(result);
+    });
+
+
+    
+    // const result = await formCollection.find({studentEmail: req.params.email}).toArray();
+
+    // const indexKeys = { title: 1, category: 1 };
+
+    // const result = await collegeCollection.createIndex(indexKeys);
+
+    // app.get(`/collegeSearchByTitle/:text`, async (req, res) => {
+    //   const searchText = req.params.text;
+    //   const result = await collegeCollection
+    //     .find({
+    //       name: { $regex: searchText, $options: "i" },
+    //     })
+    //     .toArray();
+    //   res.send(result);
+    // });
+
+    // app.get(`/categoryTab/:text`, async (req, res) => {
+    //   const text = req.params.text;
+    //   const query = { subcategory: text };
+    //   const result = await collegeCollection.find(query)
+    //     .limit(3).toArray();
+    //   res.send(result)
+    // })
+
+    // app.get("/searchText", async (req, res) => {
+    //   const searchText = req?.query?.search;
+    //   const result = await collegeCollection
+    //     .find({
+    //       name: { $regex: searchText, $options: "i" },
+    //     })
+    //     .toArray();
+    //   res.send(result);
+    // });
 
     await client.db("admin").command({ ping: 1 });
     console.log(
